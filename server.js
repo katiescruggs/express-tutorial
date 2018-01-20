705f2e896bf75817c9a5a624693293950bd9be93
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-19 20:36:18 -0700

    button with event listener

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (request, response) => {
-  response.send('hello world');
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
+const timeLogger = (request, response, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
+app.use(urlLogger, timeLogger);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/json', (request, response) => {
+  response.status(200).json({"name": "Katie"});
 });
 
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
 });
+
